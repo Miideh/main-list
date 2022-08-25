79d75933a470a68f5c439c34b97190da192c537d
Miideh <adesanyaayomide286@gmail.com>
2022-08-25 16:53:07 -0400

    improvement to v 1

--- main list.py
+++ main list.py
@@ -2,11 +2,14 @@ names = []
 max_length = 3
 
 while len(names)<max_length:
-    list = (input("enter the list of names"))
-    names.append(list)
-    print(names)
-y = (input("names"))
-if y in names:
-    print("True. The name is in the list")
+   cleaned_names=input("enter the list of names")
+   names.append(cleaned_names)
+   new_name = [cleaned_names.lower() for cleaned_names in names]
+   print(new_name)
+#for i in range(len(names)):
+ #    names[i]=names[i].lower()
+selected_names=input("names")
+if selected_names in cleaned_names:
+    print ("True. The name is in the list")
 else:
     print("False. The name cannot be found in the list")
