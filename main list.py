841af473a1307e9215919ff628b38cc91113997c
Miideh <adesanyaayomide286@gmail.com>
2022-08-29 15:33:30 -0400

    Update main list.py

--- main list.py
+++ main list.py
@@ -6,10 +6,9 @@ while len(names)<max_length:
    names.append(cleaned_names)
    new_name = [cleaned_names.lower() for cleaned_names in names]
    print(new_name)
-#for i in range(len(names)):
- #    names[i]=names[i].lower()
-selected_names=input("names")
-if selected_names in names:
+new_selected=input("name.lower() in list of names in lowercase")
+if new_selected in names:
     print ("True. The name is in the list")
 else:
     print("False. The name cannot be found in the list")
+ 
